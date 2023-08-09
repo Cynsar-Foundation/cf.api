20f581c3ff98709cd7c231e710bec143fddd3550
Saransh Sharma <saranshis@pm.me>
2023-08-09 18:45:03 +0100

    Chaged the Data Model Sourcing

--- src/api/additional-accessorie/controllers/additional-accessorie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * additional-accessorie controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::additional-accessorie.additional-accessorie');
--- src/api/additional-accessorie/services/additional-accessorie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * additional-accessorie service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::additional-accessorie.additional-accessorie');
