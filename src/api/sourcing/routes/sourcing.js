b71fddf7231bf782744c131a1abe88a39cecf2cb
Saransh Sharma <saranshis@pm.me>
2023-08-09 18:47:19 +0100

    Chaged the Data Model Sourcing

---
+++ src/api/sourcing/routes/sourcing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sourcing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sourcing.sourcing');
---
+++ src/api/sourcing/services/sourcing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sourcing service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sourcing.sourcing');
