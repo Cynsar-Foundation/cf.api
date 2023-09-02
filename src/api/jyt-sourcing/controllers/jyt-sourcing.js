d5e162dbe661322f297b80b855f1b511aacd58f9
Saransh Sharma <saranshis@pm.me>
2023-09-02 14:38:19 +0200

    Some Changes

---
+++ src/api/jyt-sourcing/controllers/jyt-sourcing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * another-sourcing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::jyt-sourcing.jyt-sourcing');
---
+++ src/api/jyt-sourcing/routes/jyt-sourcing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * another-sourcing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::jyt-sourcing.jyt-sourcing');
